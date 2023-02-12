60ad9bff75c6d9657f40fa7ac27d5e9ee9b418ae
unknown <henrytesting8@gmail.com>
2023-02-12 10:32:32 +0700

    ganti useState ke useReducer

--- src/hook/use-input.js
+++ src/hook/use-input.js
@@ -1,33 +1,61 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.value };
+  }
+
+  if (action.type === "BLUR") {
+    return { isTouched: true, value: state.value };
+  }
+
+  if (action.type === "RESET") {
+    return { isTouch: false, value: "" };
+  }
+  return inputStateReducer;
+};
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
+
+  // const [enteredValue, setEnteredValue] = useState("");
+  // const [isTouched, setIsTouched] = useState(false);
 
   //const valueIsValid = enteredValue.trim() !== "";
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+    //setEnteredValue(event.target.value);
+    dispatch({ type: "INPUT", value: event.target.value });
   };
 
   const valueBlurHandler = (event) => {
-    setIsTouched(true);
+    //setIsTouched(true);
+    dispatch({ type: "BLUR" });
   };
 
-  const resetHandler = (event) =>{
-    setEnteredValue('');
-    setIsTouched(false);
-  }
+  const resetHandler = (event) => {
+    // setEnteredValue("");
+    // setIsTouched(false);
+    dispatch({ type: "RESET" });
+  };
 
   return {
-    value: enteredValue,
+    value: inputState.value,
     valueIsValid,
     hasError,
     valueChangeHandler,
     valueBlurHandler,
-    resetHandler
+    resetHandler,
   };
 };
 
